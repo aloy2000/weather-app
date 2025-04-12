93adb3df3df722d61c35e3d3d32af84e08c7cb1d
aloy <ramanandraibenirina@gmail.com>
2025-04-12 21:01:45 +0300

    Initial Commit

---
+++ SwiftUi-Wheater/SwiftUi_WheaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_WheaterApp.swift
+//  SwiftUi-Wheater
+//
+//  Created by RAMANANDRAIBE Nirintsoa on 12/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_WheaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
